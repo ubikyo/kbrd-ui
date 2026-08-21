84059f21d6bb2bfec9a9ae74935a52baf2375dbd
Jérôme Quintard <jquintard@ubikyo.com>
2026-08-21 18:21:40 +0000

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ export default function App() {
       <AppShell.Main
         style={{
           height: "100vh",
-          backgroundColor:"#FF0000",
+          backgroundColor:"#171514",
         }}
       >
         <Box
