789e97930693b9a75f7c4c7a36538519e9ba9fac
Jérôme Quintard <jquintard@ubikyo.com>
2026-08-28 19:04:53 +0000

    wip

--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -105,6 +105,7 @@ export default function Workspace({ geometryId, onChange }: Props) {
             style={{
               width: 250,
               boxSizing: "border-box",
+              borderLeft: "1px solid var(--kbrd-border-color)",
               borderRight: "1px solid var(--kbrd-border-color)",
             }}
           >
